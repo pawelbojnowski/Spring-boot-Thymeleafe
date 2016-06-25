cb89a10e3f3c6a6783ff7bab5209882212031f59
pawel <s12021@pjwstk.edu.pl>
2016-06-25 20:12:07 +0200

    Login and password

--- src/main/java/pl/pb/web/spring/thymeleafe/WebSecurityConfig.java
+++ src/main/java/pl/pb/web/spring/thymeleafe/WebSecurityConfig.java
@@ -32,6 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("a").password("a").roles("USER");
+                .withUser("admin").password("admin").roles("USER");
     }
 }
